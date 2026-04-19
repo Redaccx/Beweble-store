983dfc76461aa7df16de92ebca872f142174061b
Redaccx <105161552+Redaccx@users.noreply.github.com>
2026-04-19 16:05:13 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration - matgary-wa-2025
+window.FIREBASE_CONFIG = {
+     apiKey: "AIzaSyABfBoajVCS8E6u8o1fJrQUuqvS9LPBM9g",
+     authDomain: "beweblestore.firebaseapp.com",
+     databaseURL: "https://beweblestore-default-rtdb.firebaseio.com",
+     projectId: "beweblestore",
+     storageBucket: "beweblestore.firebasestorage.app",
+     messagingSenderId: "392640420443",
+     appId: "1:392640420443:web:6065fbc4a00e52977d3d7e"
+ };
+
+// رقم واتساب مالك المنصة (للاشتراكات والدعم)
+window.ADMIN_PHONE = '+96181112046';
